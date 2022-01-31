89ee41fca8c4765a4714131ef33782ec18587f2f
KyungHo <akh981216@naver.com>
2022-01-31 16:54:41 +0900

    backjoon(2438)

---
+++ src/Backjoon/ForStar.java
@@ -0,0 +1,18 @@
+package Backjoon;
+
+import java.util.Scanner;
+
+public class ForStar {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		int num = stdIn.nextInt();
+		
+		for(int i=1; i<=num; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
